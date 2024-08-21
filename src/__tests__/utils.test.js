3da3eae2acbd8739aede69a9fe4aee6c2248bc8e
John Shoemaker <shoemjoh@gmail.com>
2024-08-20 21:51:40 -0400

    working test driven development lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true if the word is a palindrome and false if it's not", () => {
+        // Arrange
+        const palYes = "racecar";
+
+        // Act
+        const checkPal = isPalindrome(palYes);
+
+        // Assert
+        expect(checkPal).toBe(true);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
 // Your code here
+// Function that accepts a string as an argument
+// Returns true if the string is a palindrome and false if it isn't
+// Receive string, create new string that is all of the string characters backwards.
+// Loop through input string, i = string length, i-- , i = 0? OR i = 1, i++ and then insert it in character -1?
+
+export function isPalindrome(word) {
+    let normalizedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    //console.log(word[0])
+    let length = normalizedWord.length;
+    for (let i = 0; i < length / 2; i++) {
+        for (let i = 0; i < length / 2; i++) {
+            if (normalizedWord[i] !== normalizedWord[length - i - 1]) {
+                console.log("Not a palindrome")
+                return false;
+            }
+        }
+        return true
+
+    }
+}
